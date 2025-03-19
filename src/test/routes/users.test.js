f929178b39af1676a53a9ab21cd25fd9499f04f7
vvduth <ducthai060501@gmail.com>
2025-03-19 10:44:44 +0200

    add database connection setup in tests and reorder script commands in package.json

--- src/test/routes/users.test.js
+++ src/test/routes/users.test.js
@@ -1,17 +1,33 @@
-const request = require('supertest')
-const buildApp = require('../../app')
-const UserRepo = require('../../repos/user-repo')
+const request = require("supertest");
+const buildApp = require("../../app");
+const UserRepo = require("../../repos/user-repo");
+const pool = require("../../pool");
+beforeAll(() => {
+  pool.connect({
+    user: "postgres",
+    password: "admin",
+    database: "social",
+    host: "localhost",
+    port: 5432,
+  });
+});
 
-it('create a user', async () => {
-    const startingCount = await UserRepo.count()
-    expect(startingCount).toEqual(0)
-    await request(buildApp())
-    .post('/user')
+it("create a user", async () => {
+  const startingCount = await UserRepo.count();
+  expect(startingCount).toEqual(0);
+  await request(buildApp())
+    .post("/user")
     .send({
-        username: 'testuser', 
-        bio: 'test bio'
-    }).expect(200);
+      username: "testuser",
+      bio: "test bio",
+    })
+    .expect(200);
 
-    const finnishCount = await UserRepo.count()
-    expect(finnishCount).toEqual(1)
+  const finnishCount = await UserRepo.count();
+  expect(finnishCount).toEqual(1);
+});
+
+
+afterAll(() => {
+    return pool.close()
 })
